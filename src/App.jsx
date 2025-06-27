4c66f7c566472c81206e0169707372af5960f4ec
aziz4678 <muhamadaziz564@gmail.com>
2025-06-27 12:00:25 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center font-bold p-10 space-y-6 bg-gray-50 min-h-screen">
+        <h1 className="text-4xl font-playfair text-gray-900">
+          Hello World !!
+        </h1>
+
+        <p className="text-lg font-inter text-gray-700">
+          Hello World !!
+        </p>
+      </div>
+    </>
+  )
+
+}
+
+export default App
