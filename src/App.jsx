1168d67aff772b644f8d9f4c049b2d4c23bd0344
aziz4678 <muhamadaziz564@gmail.com>
2025-06-28 15:35:20 +0700

    Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
-import RecipeSection from "./components/RecipeSection";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
       <main className="flex-1 px-6 py-10">
-        <HeroSection />
-        <RecipeSection />
+        <Home />
       </main>
       <Footer />
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "../components/HeroSection";
+import RecipeSection from "../components/RecipeSection";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <RecipeSection />
+    </>
+  );
+}
